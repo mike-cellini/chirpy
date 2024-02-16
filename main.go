60b0991905864ca16163a3ddbf71a78a2c923450
Mike Cellini <mike.cellini@protonmail.com>
2024-02-15 23:29:10 -0600

    Tidy up

--- main.go
+++ main.go
@@ -4,41 +4,12 @@ import (
     "github.com/go-chi/chi/v5"
 
     "encoding/json"
-    "fmt"
     "log"
     "net/http"
 )
 
 type apiConfig struct {
     fileserverHits int
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        log.Print("Incrementing file server hits")
-        cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-    log.Print("Retrieving file server metrics")
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    w.WriteHeader(200)
-    var body = fmt.Sprintln(
-        "<html>\n", 
-        "<body>", 
-        "<h1>Welcome, Chirpy Admin</h1>",
-        "<p>Chirpy has been visited %d times!</p>",
-        "</body>",
-        "</html>")
-    w.Write([]byte(fmt.Sprintf(body, cfg.fileserverHits)))
-}
-
-func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
-    log.Print("Reseting file server metrics")
-    w.WriteHeader(200)
-    cfg.fileserverHits = 0
 }
 
 func main() {
